0b291e4bb4ce8d349f4286e597851e24539f9c80
CerulanLumina <CerulanLumina@users.noreply.github.com>
2019-09-11 18:23:16 -0400

    documentation and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 
+//! A simple crate for reading the data files of FFXIV.
+//!
+//! The main entry point for most use cases is the `SqPack` struct,
+//! which accepts a path to the data files on the user's machine.
+//! From an instance of this struct, you can open `Read` implementors
+//! which return the decoded data.
+//!
+//! For more advanced usage, the internal mechanisms are exposed. Typically
+//! one would start with a `SqPath` / `SqPathBuf`, pass that to a
+//! function in the `io` module, along with a path to the data files, to
+//! open `Read` streams.
 
 pub mod sqpath;
-mod io;
+pub mod io;
 
 pub use io::{
     SqpackError,
--- src/sqpath.rs
+++ src/sqpath.rs
@@ -8,6 +8,22 @@ pub struct SqPath {
 }
 
 impl SqPath {
+    /// Creates a new borrowed SqPath from a str-like input reference
+    ///
+    /// # Examples
+    /// ```
+    /// use sqpack::sqpath::SqPathBuf;
+    /// use sqpack::SqPath;
+    ///
+    /// // from an &str
+    /// let a = SqPath::new("testing");
+    ///
+    /// // from a String
+    /// let s = String::from("testing");
+    /// let b = SqPath::new(&s);
+    ///
+    /// assert_eq!(a, b)
+    /// ```
     pub fn new<S: AsRef<str> + ?Sized>(s: &S) -> &SqPath {
         // Use of unsafe follows same format as std::path::Path for unsized type
         unsafe { &*(s.as_ref() as *const str as *const SqPath) }
@@ -18,6 +34,26 @@ impl SqPath {
 /// An owned, sized representation of a location within the FFXIV data files.
 pub struct SqPathBuf {
     inner: String
+}
+
+impl SqPathBuf {
+    /// Creates a new owned & allocated SqPathBuf from a str-like input reference
+    ///
+    /// # Examples
+    /// ```
+    /// use sqpack::sqpath::SqPathBuf;
+    /// // from an &str
+    /// let a = SqPathBuf::new("testing");
+    ///
+    /// // from a String ref
+    /// let s = String::from("testing");
+    /// let b = SqPathBuf::new(&s);
+    ///
+    /// assert_eq!(a, b)
+    /// ```
+    pub fn new<S: AsRef<str> + ?Sized>(s: &S) -> SqPathBuf {
+        SqPathBuf { inner: String::from(s.as_ref()) }
+    }
 }
 
 impl AsRef<SqPath> for str {
@@ -36,4 +72,43 @@ impl AsRef<SqPath> for String {
 
 impl AsRef<SqPath> for SqPathBuf {
     fn as_ref(&self) -> &SqPath { self.inner.as_ref() }
+}
+
+#[cfg(test)]
+mod sqpath_tests {
+    use SqPath;
+    use sqpath::SqPathBuf;
+
+    #[test]
+    fn basic_sqpath() {
+        let iref = &SqPath::new("asdasd").inner;
+        assert_eq!(iref, "asdasd");
+    }
+
+    #[test]
+    fn sqpath_as_refs() {
+        let a: &SqPath = "uwu".as_ref();
+        let b: &SqPath = a.as_ref();
+        let s = String::from("uwu");
+        let c: &SqPath = s.as_ref();
+        let sqpb = SqPathBuf::new("uwu");
+        let d: &SqPath = sqpb.as_ref();
+        assert_eq!(&a.inner, "uwu");
+        assert_eq!(&b.inner, "uwu");
+        assert_eq!(&c.inner, "uwu");
+        assert_eq!(&d.inner, "uwu");
+    }
+
+    #[test]
+    fn basic_sqpathbuf() {
+        let sqpb = SqPathBuf::new("uwu");
+        assert_eq!(sqpb.inner, "uwu");
+    }
+
+    #[test]
+    fn new_params_any() {
+        SqPathBuf::new("uwu");
+        let s = String::from("uwu");
+        SqPathBuf::new(&s);
+    }
 }
