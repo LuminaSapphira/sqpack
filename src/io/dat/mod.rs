98202214c5431a1115c473f8d7db6fc6e7fa37ce
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-03-07 16:43:41 -0500

    Change organization of SqFile to be under io::dat rather than a submodule

--- src/io/dat/mod.rs
+++ src/io/dat/mod.rs
@@ -1,7 +1,8 @@
 use std::convert::TryFrom;
 use error::SqpackError;
 
-pub mod sqfile;
+mod sqfile;
+pub use self::sqfile::SqFile;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 /// The type of content stored within a file stored within a .dat file of a SqPack
